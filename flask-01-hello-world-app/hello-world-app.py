414ff2cbcfb2e20f62b709ce6712990aa98666b3
hasim7025 <kochasim25@gmail.com>
2023-11-20 21:15:23 +0100

    second commit

--- flask-01-hello-world-app/hello-world-app.py
+++ flask-01-hello-world-app/hello-world-app.py
@@ -1,12 +1,19 @@
 from flask import Flask 
 app = Flask(__name__)
 
+@app.route('/')
+def head():
+    return "hello world"
 
+@app.route('/second')
+def second():
+    return "this is the second page"
 
-
-
+@app.route('/third')
+def third():
+    return "flask-01-02/this is the third page"
 
 if __name__ == '__main__':
 
-    app.run(debug=True, port=30000)
+    app.run(debug=True, port=30000)         # normally it runs on the port:3000
     # app.run(host= '0.0.0.0', port=8081)
